c5a929fbb9924b81d42c365254591b8217b4b7dc
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-09-11 21:35:08 +0200

    add socket.io

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db');
+const socket = require('socket.io');
 
 const app = express();
+
+const messages = [];
 
 // middlewares
 app.use(express.json());
@@ -19,6 +21,19 @@ app.get('*', (req, res) => {
 });
 
 // start server
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id - ' + socket.id);
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+  console.log('I\'ve added a listener on message event \n');
 });
